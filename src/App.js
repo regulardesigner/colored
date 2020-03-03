de96898a4d7ca6b07d4ca440b1c6ab9c71f9ea0a
Damien Tailhades <damien.tailhades@gmail.com>
2020-03-03 14:15:41 +0100

    CHANGE text color depending on background hue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const randomColor = () => {
+  const pickInBetween = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  
+  const getRandom = () => {
+    return Math.floor(Math.random() * pickInBetween.length);
+  }
+  const red = getRandom();
+  const green = getRandom();
+  const blue = getRandom();
+
+  const getColor = pickInBetween[red]+pickInBetween[green]+pickInBetween[blue];
+
+  return getColor;
+}
+
+const calculateHue = (color) => {
+  // get the R G B value from HEX color
+  const r = color.substr(0,1)+color.substr(0,1)
+  const g = color.substr(1,1)+color.substr(1,1)
+  const b = color.substr(2,2)+color.substr(2,2)
+  const red = parseInt(r, 16)
+  const green = parseInt(g, 16)
+  const blue = parseInt(b, 16)
+  // 
+  const hue = red*0.299 + green*0.587 + blue*0.114;
+
+  if(color.length === 3) {
+    console.log('HUE SCORE:', hue);
+    switch (true) {
+      case (hue > 150):
+        return 'black';
+      default:
+        return 'white';
+    }
+  }
+}
+
+const App = () => {
+  let [ bckColor, setBckColor ] = useState(randomColor());
+  const [ textColor, setTextColor ] = useState(calculateHue(bckColor));
+
+  const handleColorChange = (event) => {
+    setBckColor(event.target.value)
+    calculateHue(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundColor: '#'+bckColor }}>
+      <div className="app-color" style={{ color: calculateHue(bckColor) }}>
+        #<input style={{ display: 'inline-block', color: calculateHue(bckColor) }} type="text" value={bckColor} onChange={handleColorChange}/>
+      </div>
     </div>
   );
 }
