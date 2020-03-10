8ca65222f25ab26fc62c6f06e251f87b4f9f7f8f
Damien Tailhades <damien.tailhades@gmail.com>
2020-03-10 18:39:09 +0100

    Updates

---
+++ precache-manifest.57327daff5fede6aa287f01ace7b6115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8563f20983386eb5fe1e29e9679bf83d",
+    "url": "/colored/index.html"
+  },
+  {
+    "revision": "837334c9d31ae2fff89c",
+    "url": "/colored/static/css/main.73e8f523.chunk.css"
+  },
+  {
+    "revision": "29bc7a3ea587393a32c2",
+    "url": "/colored/static/js/2.33b3e1d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/colored/static/js/2.33b3e1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837334c9d31ae2fff89c",
+    "url": "/colored/static/js/main.a8953992.chunk.js"
+  },
+  {
+    "revision": "a1e0d7716577bf4f8fe8",
+    "url": "/colored/static/js/runtime-main.0cd08814.js"
+  }
+]);
