f3292d50293580e2582ba7c4ef496aff937a6915
Damien Tailhades <damien.tailhades@gmail.com>
2020-03-10 16:33:49 +0100

    CHANGE: new responsive layout + video demo for mobile

--- src/Info.js
+++ src/Info.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Info = () => {
+const Info = (props) => {
+  const { color } = props;
   return (
     <div className="info">
-      <h2>COLORED</h2>
-      <h3>Welcome to my color paletter creator project.</h3>
+      <h2 style={{ color: `#${color}` }}>COLORED</h2>
+      <p>Hi folks, you have to create a website design but you don't know what colors to pick? Don't be nervous, COLORED is here to help you!</p>
+      <p>With COLORED you can easily create a 5 colors theme for your next website project with an ounce of serendipity.</p>
+      <p>So ask a few random colors and make a theme with those!</p>
     </div>
   )
 }
